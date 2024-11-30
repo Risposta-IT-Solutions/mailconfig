03dd2f416cb371d32dc5891c44ac3041b99cebba
Vishnu <vishnu.vijayan@elsapiens.com>
2024-11-30 18:54:33 +0530

    fix

--- cmd/step11.sh
+++ cmd/step11.sh
@@ -19,17 +19,12 @@ echo "Inserting new email into the database..."
 # Securely run the MySQL command
 mysql -u root -e "
 USE postfix_db;
-INSERT INTO virtual_users (id, domain_id, password, email) VALUES
-  (1, 1, 'a84f69cdf4c0cac5e6c8bb8043f5655b3c5ae5bd1908397c873c72a32ebff30a', '$PREFIX@$DOMAIN')
-  ON DUPLICATE KEY UPDATE password = 'a84f69cdf4c0cac5e6c8bb8043f5655b3c5ae5bd1908397c873c72a32ebff30a', email = '$PREFIX@$DOMAIN',
-  
-  (2, 1, '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'root@$DOMAIN')
-  ON DUPLICATE KEY UPDATE password = '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', email = 'root@$DOMAIN';
+DELETE FROM virtual_users WHERE email = '$PREFIX@$DOMAIN' AND email = 'root@$DOMAIN';
+INSERT INTO virtual_users (domain_id, password, email) VALUES 
+  (1, 'a84f69cdf4c0cac5e6c8bb8043f5655b3c5ae5bd1908397c873c72a32ebff30a', '$PREFIX@$DOMAIN'),
+  (1, '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'root@$DOMAIN');
 EXIT;
 "
-
-
-
 
 # Check if MySQL query was successful
 if [ $? -eq 0 ]; then
