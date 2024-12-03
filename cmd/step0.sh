caf9745131c6c84725c35a8fbeb229c03a5797f4
Vishnu <vishnu.vijayan@elsapiens.com>
2024-12-03 15:10:54 +0530

    update

--- cmd/step0.sh
+++ cmd/step0.sh
@@ -24,35 +24,4 @@ echo "Configuration file '$CONF_FILE' created with the following content:"
 cat $CONF_FILE
 
 
-#!/bin/bash
-
-
-LOCK_FILE="/var/lib/dpkg/lock-frontend"
-
-# Function to kill the process holding the lock
-release_lock() {
-  # Get the PID of the process holding the lock
-  LOCK_PID=$(lsof $LOCK_FILE | awk 'NR==2 {print $2}')
-  
-  if [ -n "$LOCK_PID" ]; then
-    echo "Process $LOCK_PID is holding the lock. Killing it..."
-    kill -9 $LOCK_PID
-    if [ $? -eq 0 ]; then
-      echo "Successfully killed process $LOCK_PID."
-    else
-      echo "Failed to kill process $LOCK_PID. Exiting."
-      exit 1
-    fi
-  else
-    echo "No process is holding the lock."
-  fi
-}
-
-if [ -f "$LOCK_FILE" ]; then
-  echo "Lock file detected: $LOCK_FILE"
-  release_lock
-else
-  echo "No lock file detected."
-fi
-
 
