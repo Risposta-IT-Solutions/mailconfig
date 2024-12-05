973873986f78caa70288c7ab2bf9b73b0bfd420e
Vishnu Vijayan <vishnu.vijayan@elsapiens.com>
2024-12-05 22:16:44 +0530

    roundcube user

--- cmd/step6.sh
+++ cmd/step6.sh
@@ -7,6 +7,20 @@ else
   exit 1
 fi
 
+DB_NAME="roundcube"
+DB_USER="roundcube"
+DB_PASSWORD="Zz9730TH"
+
+
+mysql -u root <<EOF
+CREATE DATABASE IF NOT EXISTS $DB_NAME;
+CREATE USER IF NOT EXISTS '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';
+FLUSH PRIVILEGES;
+EXIT;
+EOF
+
+
 # Step 1: Create directories and generate DKIM keys
 echo "Creating DKIM directories and generating keys for $DOMAIN..." > /home/logs/step6.log
 sudo mkdir -p /etc/opendkim/keys/$DOMAIN
