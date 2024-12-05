4a841d2cb40bc5c1dc8a3a1e21aaf511d17a33ff
Vishnu Vijayan <vishnu.vijayan@elsapiens.com>
2024-12-05 22:29:13 +0530

    fix

--- cmd/step6.sh
+++ cmd/step6.sh
@@ -12,12 +12,11 @@ DB_USER="roundcube"
 DB_PASSWORD="Zz9730TH"
 
 
-mysql -u root <<EOF
+mysql -u root <<-EOF
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER IF NOT EXISTS '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';
 FLUSH PRIVILEGES;
-EXIT;
 EOF
 
 echo "Created database '$DB_NAME' and user '$DB_USER' successfully." > /home/logs/step6.log;
