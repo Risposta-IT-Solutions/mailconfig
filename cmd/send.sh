c11489aeb5a999522aeeaf8f064dc346666bc466
Vishnu Vijayan <vishnu.vijayan@elsapiens.com>
2025-01-10 19:00:09 +0530

    local env

--- cmd/send.sh
+++ cmd/send.sh
@@ -1,55 +1,63 @@
 #!/bin/bash
 
 
-if [[ ! -f "/home/config.env" ]]; then
-    echo "Error: Configuration file not found - Skipping log request"
-else 
+if [[ -f "/home/config.env" ]]; then
     source /home/config.env
-
-    # Check if a message is provided
-    if [ -z "$1" ]; then
-        echo "Error: No message provided"
-        exit 1
+else 
+    DOMAIN="0"
+    tmp_conf_file="/home/tmp.env"
+    if [[ -f "$tmp_conf_file" ]]; then
+        source $tmp_conf_file
     fi
+fi 
 
-    if [ "$ENVIRONMENT" == "production" ]; then
-        URL="https://api.pay-per-lead.co.uk/mailConfig/log"
-    else
-        URL="https://beta.api.pay-per-lead.co.uk/mailConfig/log"
-    fi
+IP=$(hostname -I | awk '{print $1}')
 
-    if ! command -v jq > /dev/null 2>&1; then
-    echo "jq not found. Installing jq..."
-    sudo apt update && sudo apt install -y jq
-    fi
-
-    if ! command -v curl > /dev/null 2>&1; then
-    echo "curl not found. Installing curl..."
-    sudo apt update && sudo apt install -y curl
-    fi
-
-    # Escape special characters in the message
-    escaped_message=$(printf '%s' "$1" | jq -R .)
-
-    # Build JSON data
-    DATA=$(jq -n \
-        --arg ip "$IP" \
-        --arg domain "$DOMAIN" \
-        --arg message "$escaped_message" \
-        '{"ip": $ip, "domain": $domain, "message": $message}')
-
-    # Send POST request
-    response=$(curl -s -o /dev/null -w "%{http_code}" -X POST \
-        -H "Content-Type: application/json" \
-        -d "$DATA" "$URL")
-
-    # Check the HTTP status code
-    if [ "$response" -eq 200 ]; then
-        echo "Log request successful"
-        exit 0
-    else
-        echo "Log request failed with status code $response"
-        exit 1
-    fi
-
+# Check if a message is provided
+if [ -z "$1" ]; then
+    echo "Error: No message provided"
+    exit 1
 fi
+
+if [ "$ENVIRONMENT" == "production" ]; then
+    URL="https://api.pay-per-lead.co.uk/mailConfig/log"
+elif [ "$ENVIRONMENT" == "local" ]; then
+    URL="http://payperlead.loc/mailConfig/log"
+else
+    URL="https://beta.api.pay-per-lead.co.uk/mailConfig/log"
+fi
+
+if ! command -v jq > /dev/null 2>&1; then
+echo "jq not found. Installing jq..."
+sudo apt update && sudo apt install -y jq
+fi
+
+if ! command -v curl > /dev/null 2>&1; then
+echo "curl not found. Installing curl..."
+sudo apt update && sudo apt install -y curl
+fi
+
+# Escape special characters in the message
+escaped_message=$(printf '%s' "$1" | jq -R .)
+
+# Build JSON data
+DATA=$(jq -n \
+    --arg ip "$IP" \
+    --arg domain "$DOMAIN" \
+    --arg message "$escaped_message" \
+    '{"ip": $ip, "domain": $domain, "message": $message}')
+
+# Send POST request
+response=$(curl -s -o /dev/null -w "%{http_code}" -X POST \
+    -H "Content-Type: application/json" \
+    -d "$DATA" "$URL")
+
+# Check the HTTP status code
+if [ "$response" -eq 200 ]; then
+    echo "Log request successful"
+    exit 0
+else
+    echo "Log request failed with status code $response"
+    exit 1
+fi
+
--- cmd/status.sh
+++ cmd/status.sh
@@ -1,54 +1,61 @@
 #!/bin/bash
-if [[ ! -f "/home/config.env" ]]; then
-    echo "Error: Configuration file not found - Skipping status update"
-else 
-
+if [[ -f "/home/config.env" ]]; then
     source /home/config.env
-
-    # Check if a status is provided
-    if [ -z "$1" ]; then
-        echo "Error: No status provided"
-        exit 1
+else 
+    DOMAIN="0"
+    tmp_conf_file="/home/tmp.env"
+    if [[ -f "$tmp_conf_file" ]]; then
+        source $tmp_conf_file
     fi
+fi 
 
-    if [ "$ENVIRONMENT" == "production" ]; then
-        URL="https://api.pay-per-lead.co.uk/mailConfig/updateStatus"
-    else
-        URL="https://beta.api.pay-per-lead.co.uk/mailConfig/updateStatus"
-    fi
+IP=$(hostname -I | awk '{print $1}')
 
-    if ! command -v jq > /dev/null 2>&1; then
-    echo "jq not found. Installing jq..."
-    sudo apt update && sudo apt install -y jq
-    fi
-
-    if ! command -v curl > /dev/null 2>&1; then
-    echo "curl not found. Installing curl..."
-    sudo apt update && sudo apt install -y curl
-    fi
-
-    # Escape special characters in the message
-    escaped_message=$(printf '%s' "$1" | jq -R .)
-
-    # Build JSON data
-    DATA=$(jq -n \
-        --arg ip "$IP" \
-        --arg domain "$DOMAIN" \
-        --arg status "$escaped_message" \
-        '{"ip": $ip, "domain": $domain, "status": $status}')
-
-    # Send POST request
-    response=$(curl -s -o /dev/null -w "%{http_code}" -X POST \
-        -H "Content-Type: application/json" \
-        -d "$DATA" "$URL")
-
-    # Check the HTTP status code
-    if [ "$response" -eq 200 ]; then
-        echo "Status update request successful"
-        exit 0
-    else
-        echo "Status update request failed with status code $response"
-        exit 1
-    fi
-
+# Check if a status is provided
+if [ -z "$1" ]; then
+    echo "Error: No status provided"
+    exit 1
 fi
+
+if [ "$ENVIRONMENT" == "production" ]; then
+    URL="https://api.pay-per-lead.co.uk/mailConfig/updateStatus"
+elif [ "$ENVIRONMENT" == "local" ]; then
+    URL="http://payperlead.loc/mailConfig/updateStatus"
+else
+    URL="https://beta.api.pay-per-lead.co.uk/mailConfig/updateStatus"
+fi
+
+if ! command -v jq > /dev/null 2>&1; then
+echo "jq not found. Installing jq..."
+sudo apt update && sudo apt install -y jq
+fi
+
+if ! command -v curl > /dev/null 2>&1; then
+echo "curl not found. Installing curl..."
+sudo apt update && sudo apt install -y curl
+fi
+
+# Escape special characters in the message
+escaped_message=$(printf '%s' "$1" | jq -R .)
+
+# Build JSON data
+DATA=$(jq -n \
+    --arg ip "$IP" \
+    --arg domain "$DOMAIN" \
+    --arg status "$escaped_message" \
+    '{"ip": $ip, "domain": $domain, "status": $status}')
+
+# Send POST request
+response=$(curl -s -o /dev/null -w "%{http_code}" -X POST \
+    -H "Content-Type: application/json" \
+    -d "$DATA" "$URL")
+
+# Check the HTTP status code
+if [ "$response" -eq 200 ]; then
+    echo "Status update request successful"
+    exit 0
+else
+    echo "Status update request failed with status code $response"
+    exit 1
+fi
+
