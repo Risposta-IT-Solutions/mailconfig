899b3f8f7c17ace855fa5d5266ad7615ec3b10d6
Vishnu Vijayan <vishnu.vijayan@elsapiens.com>
2025-01-15 16:07:24 +0530

    update

--- cmd/delete_email.sh
+++ cmd/delete_email.sh
@@ -16,14 +16,23 @@ email=$(echo $email | sed 's/\"//g')
 # Split email to get domain and prefix
 IFS='@' read -r PREFIX DOMAIN <<< "$email"
 
-# Remove the email directory
-sudo rm -rf /var/mail/vhosts/$DOMAIN/$PREFIX
-
 echo "" > /home/delete_email.log
 
-if [ $? -ne 0 ]; then
-  echo "Failed to delete the directory for $email." >> /home/delete_email.log
-  exit 1
+echo "Deleting mail for $email." >> /home/delete_email.log
+echo "Domain: $DOMAIN" >> /home/delete_email.log
+echo "Prefix: $PREFIX" >> /home/delete_email.log
+
+# Remove the email directory if it exists
+if [ -d "/var/mail/vhosts/$DOMAIN/$PREFIX" ]; then
+  sudo rm -rf /var/mail/vhosts/$DOMAIN/$PREFIX
+
+  if [ $? -ne 0 ]; then
+    echo "Failed to delete the directory for $email." >> /home/delete_email.log
+    exit 1
+  fi
+
+else
+  echo "Directory does not exist for $email." >> /home/delete_email.log
 fi
 
 (cd /home/mailconfig/cmd && ./flush_email.sh "$email")
