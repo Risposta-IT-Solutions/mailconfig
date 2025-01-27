5e7f5efaabedf2c27f9dd5786d3aff88b804d327
Vishnu Vijayan <vishnu.vijayan@elsapiens.com>
2025-01-27 21:57:23 +0530

    php PPA

--- cmd/setup.sh
+++ cmd/setup.sh
@@ -79,6 +79,12 @@ fi
 
 echo "Mailutils has been installed successfully!" >> $LOG_FILE
 
+sudo apt update -y
+sudo apt install -y software-properties-common
+sudo add-apt-repository -y ppa:ondrej/php
+sudo apt update -y
+
+
 apt-get install -y php8.1 php8.1-common php8.1-cli php8.1-fpm php8.1-mbstring php8.1-xml php8.1-curl php8.1-zip php8.1-gd php8.1-intl php8.1-soap php8.1-opcache php8.1-readline php8.1-mysql ca-certificates certbot python3-certbot-apache opendkim opendkim-tools libapache2-mod-php
 
 if [ $? -ne 0 ]; then
